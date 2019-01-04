0826cb3af94a54267b17638334d855b1deeeae49
Rakugen <simonlee0306@gmail.com>
2019-01-04 14:08:02 -0500

    fixed login-create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      @errors = @user.errors.full_messages
+      flash[:notice] = 'Invalid username or password'
       render :new
     end
   end
